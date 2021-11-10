c6449299b2f1d01b8e041d2c28c5edc3ee16f527
Opilane TTHK <opilane@tthk.ee>
2021-11-10 12:38:45 +0200

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+ 
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string rootpath = @"C:\Users\opilane\source\repos\Demo\FileSystem";
+           var files = Directory.GetFiles(rootpath, "*.txt", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);  
+            }
+            Console.ReadLine();
+        }
+    }
+}
